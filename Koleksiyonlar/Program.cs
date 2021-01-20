cf959956f1e63f50895d6d8197f085efbca5db08
Ömer_faruk <faruk666bozlak@gmail.com>
2021-01-20 23:49:44 +0300

    20.01.2021

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {   // listelere sonradan elaman eklenmez. bunun için bu yöntem kullanılır.
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            isimler.Add("İlker");
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; } // snippet --> hazır kodlar 
+        public int CategoryId { get; set; }
+
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; } // birim fiyatı
+        public int UnitInStock { get; set; }
+    }
+}
